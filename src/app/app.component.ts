c668f3bccb986dbec2bf0900af6f20260354bdbe
Yoli-Buendia <bue17003@byui.edu>
2024-01-27 18:28:27 -0600

    documents, messages addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'W02-assignment';
+  selectedFeature: string;
+
+  switchView(selectedFeature: string){
+    if (this.selectedFeature?.toUpperCase() !== selectedFeature.toUpperCase()){
+      this.selectedFeature = selectedFeature;
+    }
+  }
 }
